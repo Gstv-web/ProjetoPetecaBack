cd54fcac55e89682c86e23bf5523f6dd98444e6c
Paulo <pmarsiglia93@gmail.com>
2022-01-26 11:31:23 -0300

    @pmarsiglia93 [TASK: CONTROLLER, REPOSITORY DO USER]

--- App/src/main/java/com/projetoIntegrador/App/controllers/UserController.java
+++ App/src/main/java/com/projetoIntegrador/App/controllers/UserController.java
@@ -1,7 +1,61 @@
 package com.projetoIntegrador.App.controllers;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetoIntegrador.App.models.User;
+import com.projetoIntegrador.App.repositories.UserRepository;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin("*")
 public class UserController {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@GetMapping("/all")
+	public ResponseEntity<List<User>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> GetById(@PathVariable long id){
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/{user}")
+	public ResponseEntity<List<User>> GetByUser(@PathVariable String user){
+		return ResponseEntity.ok(repository.findAllByRazaoSocialContainingIgnoreCase(user));
+	}
+	
+	@PostMapping("/new")
+	public ResponseEntity<User> newUser(@RequestBody User newUser){
+		return ResponseEntity.status(201).body(repository.save(newUser));
+	}
+	
+	@PutMapping("/edit")
+	public ResponseEntity<User> editUser(@RequestBody User newUser){
+		return ResponseEntity.status(200).body(repository.save(newUser));
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public void deleteCategoria(@PathVariable long id) {
+		repository.deleteById(id);
+	}	
 
 	
 }
--- App/src/main/java/com/projetoIntegrador/App/repositories/UserRepository.java
+++ App/src/main/java/com/projetoIntegrador/App/repositories/UserRepository.java
@@ -1,9 +1,16 @@
 package com.projetoIntegrador.App.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.projetoIntegrador.App.models.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
+	
+	public List<User> findAllByRazaoSocialContainingIgnoreCase (String razaoSocial);	
+	
 
 }
