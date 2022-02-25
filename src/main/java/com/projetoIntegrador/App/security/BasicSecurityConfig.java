dc81a22bea8ff5f280fe6ae1a5dffd4b7a661aae
José Gustavo Oliveira da Silva <gustavo.myw@live.com>
2022-02-25 15:26:20 -0300

    @Gstv-web Mudança de caminho de security

--- src/main/java/com/projetoIntegrador/App/security/BasicSecurityConfig.java
+++ src/main/java/com/projetoIntegrador/App/security/BasicSecurityConfig.java
@@ -31,8 +31,8 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/user/logar").permitAll()
-		.antMatchers("/user/cadastrar").permitAll()
+		.antMatchers("/user/login").permitAll()
+		.antMatchers("/user/sign").permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
